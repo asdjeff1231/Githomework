d54bd1dad8e5dd019ca002d32147e87113030449
wwwww <asdjeff1231@gmail.com>
2018-05-17 22:59:27 +0800

    wwwww

---
+++ githomework.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+	int i; 
+	for(i=1;i<11;i++) //for迴圈做10次 
+	{
+		printf("%d ",i);//每做一次 印出目前值 
+	}
+	
+	
+	return 0;
+}
+
